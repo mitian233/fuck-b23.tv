cb577f77cdcd9ed4d15605b120d28f5da38f331d
mitian233 <2304254165@qq.com>
2022-11-07 23:48:38 +0800

    Beautify

--- index.py
+++ index.py
@@ -2,10 +2,12 @@ from flask import Flask, redirect, render_template, request
 from urllib.parse import urlparse
 import requests
 
+ua = {'User-Agent': 'Mozilla/5.0'}
+
 app = Flask(__name__)
 
 def getB23Url(url):
-    r = requests.get(url, allow_redirects=False)
+    r = requests.get(url, allow_redirects=False, headers=ua)
     url_result = urlparse(r.headers['Location'])
     genUrl = 'https://' + url_result.hostname + url_result.path
     return genUrl
@@ -14,7 +16,7 @@ def getB23Url(url):
 def homePage():  # put application's code here
     return render_template('index.html')
 
-@app.route('/<string:b23Id>')
+@app.route('/<string:b23Id>',methods=['POST','GET'])
 def dispBVNum(b23Id):
     url = 'https://b23.tv/' + b23Id
     genUrl = getB23Url(url)
@@ -28,14 +30,13 @@ def getUrlGUI():
         if not url.startswith('http'):
            url = "https://" + url
         genUrl = getB23Url(url)
-        return genUrl
     else:
         print(2)
         url = request.args.get('url')
         if not url.startswith('http'):
            url = "https://" + url
         genUrl = getB23Url(url)
-        return genUrl
+    return render_template('result.html',url=genUrl)
 
 if __name__ == '__main__':
     app.run()
