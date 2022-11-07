4ab0184a5c5061e617daa14d15e148a305f0bf89
mitian233 <2304254165@qq.com>
2022-11-07 23:12:52 +0800

    rename

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, render_template, request
 from urllib.parse import urlparse
 import requests
 
